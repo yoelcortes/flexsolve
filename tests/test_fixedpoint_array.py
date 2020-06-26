813260cff4a199290278b7a9394967f09b230bd5
Yoel <yoelcortes@gmail.com>
2020-06-26 12:22:20 -0500

    modify tests

--- tests/test_fixedpoint_array.py
+++ tests/test_fixedpoint_array.py
@@ -7,6 +7,7 @@ regarding chemical recycle loops with reactions.
 import numpy as np
 import flexsolve as flx
 from matplotlib import pyplot as plt
+from numpy import allclose
 
 conversion = 0.8
 stoichiometry = np.array([-1, 0.5, 2, 1, 0.1, 0.001, 0, 0])
@@ -33,20 +34,21 @@ def create_plot(line=False):
     if line:
         plt.plot(xs, ys)
 
+original_feed = feed.copy()
 p = flx.Profiler(f)
 flx.wegstein(p, feed, xtol=1e-8)
-print(feed)
+assert allclose(feed, original_feed)
 p.archive('Wegstein')
 flx.aitken(p, feed, xtol=1e-8,maxiter=200)
-print(feed)
+assert allclose(feed, original_feed)
 p.archive('Aitken')
 flx.fixed_point_lstsq(p, feed, xtol=5e-8, maxiter=200)
-print(feed)
+assert allclose(feed, original_feed)
 p.archive('Lstsq')
 flx.fixed_point(p, feed, xtol=5e-8, maxiter=200)
-print(feed)
+assert allclose(feed, original_feed)
 p.archive('Fixed point')
-print(p.sizes())
+assert p.sizes() == {'Wegstein': 5, 'Aitken': 5, 'Lstsq': 22, 'Fixed point': 194}
 
 flx.speed_up()
 flx.wegstein(f, feed, xtol=5e-8)
