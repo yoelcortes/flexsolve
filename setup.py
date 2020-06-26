f3e711d03dcc11863d21f7b85275b02c57f69f2f
Yoel <yoelcortes@gmail.com>
2020-06-26 14:59:56 -0500

    bumb up version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name='flexsolve',
     packages=['flexsolve'],
     license='MIT',
-    version='0.3.25',
+    version='0.3.26',
     description='Flexible function solvers',
     long_description=open('README.rst').read(),
     author='Yoel Cortes-Pena',
