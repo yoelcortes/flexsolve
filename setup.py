ac67e7ce8cba2db6c5afc9de5971a0c5df396113
Yoel <yoelcortes@gmail.com>
2024-05-24 10:30:27 -0500

    0.5.5 release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name='flexsolve',
     packages=['flexsolve'],
     license='MIT',
-    version='0.5.4',
+    version='0.5.5',
     description='Flexible function solvers',
     long_description=open('README.rst').read(),
     author='Yoel Cortes-Pena',
